2eab8c4dd85f92aac147977daf24e58993760978
Renan Tomazoni Pinzon <rpinzon@gmail.com>
2018-08-10 21:17:34 -0300

    database

--- src/main/java/org/rpinzon/snapper/model/Data.java
+++ src/main/java/org/rpinzon/snapper/model/Data.java
@@ -1,12 +1,21 @@
 package org.rpinzon.snapper.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * Created by Renan T. Pinzon on 09/08/18.
  */
+@Entity
+@Table(name = "MODEL_DATA")
 public class Data {
 
+    @Id
     private Integer pk;
 
+    @Column(nullable = false)
     private Integer score;
 
     public Data() {
